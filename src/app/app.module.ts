c483e53d3ac482b9896e74487116f3739bc06f73
rajibgiri <giri.rajib@hotmail.com>
2019-04-27 17:45:46 -0400

    Newly created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { TaskBoardComponent } from './task-board/task-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    TaskBoardComponent
   ],
   imports: [
     BrowserModule
